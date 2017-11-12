31d4addf2ad0b4b4c34d287fd41a455ae917a3b7
rogeriolaguilar <rogerio.l.aguilar@gmail.com>
2017-11-11 22:06:34 -0200

    redoing people table migration

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,28 @@
 module.exports = {
-  test: {
+
+  development: {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
     },
     useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      host: '127.0.0.1',
+      user: 'bank',
+      password: 'bank',
+      database: 'db_bank'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
 
   production: {
@@ -16,8 +34,8 @@ module.exports = {
       database: 'db_bank'
     },
     pool: {
-      min: 1,
-      max: 20
+      min: 2,
+      max: 10
     },
     migrations: {
       tableName: 'knex_migrations'
--- migrations/20171111215009_create_people_table.js
+++ migrations/20171111215009_create_people_table.js
@@ -1,20 +1,20 @@
-
-exports.up = function (knex, Promise) {
+exports.up = function (knex) {
   let schema = knex.schema.createTableIfNotExists('people', function (table) {
     table.bigIncrements('id').primary()
-    table.uuid('uuid').notNullable()
+    table.string('cpf').notNullable()
     table.string('name').notNullable()
     table.date('birthday').notNullable()
-    table.unique(['uuid'])
-    table.index(['uuid'])
+    table.timestamps(false, true)
+    table.unique('cpf')
   });
 
+  // eslint-disable-next-line no-console
   console.log(schema.toString());
   return schema;
 }
 
-exports.down = function (knex, Promise) {
+exports.down = function (knex) {
   return knex.schema.dropTable('people', function (table) {
-    table.dropIndex(['uuid'])
+    table.dropIndex(['cpf'])
   });
 }
