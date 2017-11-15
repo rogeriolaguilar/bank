1a818c8d5f44d2e80fc57ee2e80a4f0419957d4e
rogeriolaguilar <rogerio.l.aguilar@gmail.com>
2017-11-15 01:37:26 -0200

    person account adapter

--- app/account/infra/web/adapter/account_web_adapter.js
+++ app/account/infra/web/adapter/account_web_adapter.js
@@ -1,22 +1,25 @@
 const Account = require('../../../../account/domain/account')
+const PersonRequester = require('../../../../person/domain/person_requester')
 const RepositoryFactory = require('../../../../repository_factory')
 const EventProcessor = require('../../../../event/domain/event_processor')
 const AccountCreationEvent = require('../../../../event/domain/account_creation_event')
 
-class CreateAccount {
+class CreatePersonAccount {
 
   constructor(
     domainPort = new EventProcessor(RepositoryFactory.eventRepository()),
-    repository = RepositoryFactory.accountRepository()
+    repository = RepositoryFactory.accountRepository(), 
+    personRequester = new PersonRequester()
   ) {
     this._domainPort = domainPort
     this._repository = repository
+    this._personRequester = personRequester
   }
 
   create(params) {
     let account = new Account(params, this._repository)
 
-    return this._domainPort.process(new AccountCreationEvent(account))
+    return this._domainPort.process(new AccountCreationEvent(account, this._personRequester.getPerson(params.cpf)))
       .catch((e) => {
         console.log(`AccountWebAdapter.Create error code: ${e.code}, message: ${e.message}, params: ${params.cpf}`)
         throw e
@@ -25,5 +28,5 @@ class CreateAccount {
 }
 
 module.exports = {
-  CreateAccount: CreateAccount
+  CreatePersonAccount: CreatePersonAccount
 }
--- routes/accounts.js
+++ routes/accounts.js
@@ -9,9 +9,9 @@ const AccountsHandler = {
   deactivate(req, res) { res.status(204).send() }
 }
 
-router.get('/people/:personId/accounts', AccountsHandler.getPersonAccounts)
+router.get('/people/:cpf/accounts', AccountsHandler.getPersonAccounts)
 router.get('/companies/:companyId/accounts', AccountsHandler.getCompanyAccounts)
-router.post('/people/:personId/accounts', AccountsHandler.createToPerson)
+router.post('/people/:cpf/accounts', AccountsHandler.createToPerson)
 router.post('/companies/:companyId/accounts', AccountsHandler.createToPerson)
 router.delete('/accounts/:id', AccountsHandler.deactivate)
 
